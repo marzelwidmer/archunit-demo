11773ad586766e010c7c66088db949f923afe42d
Marcel Widmer <marzelwidmer@gmail.com>
2022-10-09 09:12:58 +0200

    first commit

---
+++ src/main/kotlin/ch/keepcalm/demo/archunit/ArchunitDemoApplication.kt
@@ -0,0 +1,11 @@
+package ch.keepcalm.demo.archunit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchunitDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchunitDemoApplication>(*args)
+}
---
+++ src/test/kotlin/ch/keepcalm/demo/archunit/ArchunitDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.keepcalm.demo.archunit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchunitDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
